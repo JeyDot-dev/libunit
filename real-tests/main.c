e249fea8f39cd3b0671b37ffa3aa78a170c2afc2
Stuart Rapoport <stuartrapop@gmail.com>
2023-02-12 17:16:00 +0100

    final work

--- real-tests/main.c
+++ real-tests/main.c
@@ -6,7 +6,7 @@
 /*   By: srapopor <srapopor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 11:47:00 by srapopor          #+#    #+#             */
-/*   Updated: 2023/02/12 16:53:08 by srapopor         ###   ########.fr       */
+/*   Updated: 2023/02/12 17:09:27 by srapopor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,10 @@ int	main(void)
 {
 	int	routine_response;
 
-	strdup_launcher();
 	routine_response = 0;
 	if (strlen_launcher() == -1)
+		routine_response = -1;
+	if (strdup_launcher() == -1)
 		routine_response = -1;
 	if (atoi_launcher() == -1)
 		routine_response = -1;
